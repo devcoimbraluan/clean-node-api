0fbbbc4ebd2e08187d8b1720f5aaa553afbad412
Luan <luan.rocha@distribuidora.com.br>
2021-07-07 15:41:54 -0400

    refactor: move all error imports to a single file

--- src/presentation/controllers/signup.ts
+++ src/presentation/controllers/signup.ts
@@ -4,13 +4,16 @@ import { badRequest } from '../helpers/http-helper'
 import { Controller } from '../protocols/controller'
 import { EmailValidator } from "../protocols/email-validator"
 import { InvalidParamError } from '../errors/invalid-params-error'
+import { ServerError } from '../errors/server-error'
 
 export class SignUpController implements Controller {
     private readonly emailValidator: EmailValidator
     constructor(emailValidator: EmailValidator) {
         this.emailValidator = emailValidator
     }
+
     handle (httpRequest: httpRequest): httpResponse {
+        try {
         const requiredFields = ['name', 'email', 'password', 'passwordConfirmation']
         for (const field of requiredFields) {
             if(!httpRequest.body[field]) {
@@ -20,6 +23,11 @@ export class SignUpController implements Controller {
         const isvValid = this.emailValidator.isValid(httpRequest.body.email)
         if (!isvValid) {
             return badRequest(new InvalidParamError('email'))
-        }
+        }} catch (error) {
+            return {
+                statusCode: 500,
+                body: new ServerError()
+            } 
+        } 
     }
 }
