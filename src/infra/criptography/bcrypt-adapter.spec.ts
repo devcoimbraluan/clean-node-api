1b18945351d64d53933f0446e7f82a7bc33f608f
Luan <luan.rocha@distribuidora.com.br>
2021-07-12 13:50:50 -0400

    feat: ensure AccountMongoRepository returns an account on succes

--- src/infra/criptography/bcrypt-adapter.spec.ts
+++ src/infra/criptography/bcrypt-adapter.spec.ts
@@ -1,5 +1,4 @@
 import bcrypt from 'bcryptjs'
-import { isJSDocAugmentsTag } from 'typescript'
 import {BcryptAdapter} from './bcrypt-adapter'
 
 jest.mock('bcrypt', () => ({
@@ -29,7 +28,11 @@ describe('Bcrypt Adapter', () => {
 
     test ('Should throw if bcrypt throws', async () => {
         const sut = makeSut()
-        jest.spyOn(bcrypt, 'hash').mockReturnValueOnce(new Promise((resolve, reject) => reject(new Error())))
+        jest.spyOn(bcrypt, 'hash').mockImplementationOnce(
+            () => {
+                throw new Error()
+            }
+        )
         const promise = sut.encrypt('any_value')
         await expect (promise).rejects.toThrow ()
     })
--- src/infra/db/mongodb/helpers/mongo-helper.ts
+++ src/infra/db/mongodb/helpers/mongo-helper.ts
@@ -1,9 +1,9 @@
-import { MongoClient } from "mongodb";
+import { Collection, MongoClient } from "mongodb";
 
 export const MongoHelper = {
-    lient: null as MongoClient,
+    client: null as MongoClient,
 
-    async connect (uri: string) {
+    async connect (uri: string): Promise<void> {
         this.client = await MongoClient.connect(process.env.MONGO_URL, {
             useNewUrlParser: true,
             useUnifiedTopology: true
@@ -12,5 +12,8 @@ export const MongoHelper = {
 
     async disconnect (): Promise<void> {
         await this.client.close()
+    },
+ getCollection (name: string): Collection {
+        return this.client.db().collection(name)
     }
 }
